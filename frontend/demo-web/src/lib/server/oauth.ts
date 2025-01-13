2f32d9894b73f7b32ddd62af9d71d11dfa79a492
Josef Sima <c_jsima@groupon.com>
2025-01-13 09:19:44 +0100

    env update

--- frontend/demo-web/src/lib/server/oauth.ts
+++ frontend/demo-web/src/lib/server/oauth.ts
@@ -1,8 +1,8 @@
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { Google } from "arctic";
 
 export const google = new Google(
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
+  env.GOOGLE_CLIENT_ID,
+  env.GOOGLE_CLIENT_SECRET,
   "http://localhost:5173/login/google/callback"
 );
