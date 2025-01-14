70a03127950a56cdc7a00d11c953081a2e1b16de
Josef Sima <c_jsima@groupon.com>
2025-01-14 23:23:22 +0100

    Tunned cron for each env

--- backend/article/article.cron.ts
+++ backend/article/article.cron.ts
@@ -1,16 +1,25 @@
-import { errorHandler } from "@shared/errors";
-import { api } from "encore.dev/api";
+import { appMeta } from "encore.dev";
+import { api, APIError } from "encore.dev/api";
 import { CronJob } from "encore.dev/cron";
 import { article, google_chat } from "~encore/clients";
 
 export const sendPublishedArticlesCount = api({}, async () => {
   try {
     const response = await article.count();
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+    });
+
     return await google_chat.sendMessage({
-      message: `There are ${response.count} published articles`,
+      message: `${formattedDate} we have ${
+        response.count
+      } published articles in ${resolveRegion(appMeta().environment.name)}`,
     });
   } catch (error) {
-    return errorHandler(error, "Failed to send published articles count");
+    throw APIError.internal("Failed to send published articles count");
   }
 });
 
@@ -20,3 +29,18 @@ new CronJob("published-articles", {
   schedule: "0 9 * * *",
   endpoint: sendPublishedArticlesCount,
 });
+
+function resolveRegion(environment: string) {
+  switch (environment) {
+    case "prod-eu":
+      return "EU (production)";
+    case "prod-us":
+      return "US (production)";
+    case "staging-us":
+      return "US (staging)";
+    case "staging-eu":
+      return "EU (staging)";
+    default:
+      return "local";
+  }
+}
