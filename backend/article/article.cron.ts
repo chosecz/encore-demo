ed60b2caed269ba18de6bae1f7a4553b553f290b
Josef Sima <c_jsima@groupon.com>
2025-01-14 11:06:21 +0100

    Add error handler to cron

--- backend/article/article.cron.ts
+++ backend/article/article.cron.ts
@@ -1,12 +1,17 @@
+import { errorHandler } from "@shared/errors";
 import { api } from "encore.dev/api";
 import { CronJob } from "encore.dev/cron";
 import { article, google_chat } from "~encore/clients";
 
 export const sendPublishedArticlesCount = api({}, async () => {
-  const response = await article.count();
-  return google_chat.sendMessage({
-    message: `There are ${response.count} published articles`,
-  });
+  try {
+    const response = await article.count();
+    return await google_chat.sendMessage({
+      message: `There are ${response.count} published articles`,
+    });
+  } catch (error) {
+    return errorHandler(error, "Failed to send published articles count");
+  }
 });
 
 // Sends notification with number of published articles every day at 9:00 AM
